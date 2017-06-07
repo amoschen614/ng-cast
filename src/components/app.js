fd858a649cf9fca1cc6d594ca8683a435afb1664
Hack Reactor Student <student@hackreactor.com>
2017-06-06 17:11:18 -0700

    Implement video search through YouTube API request

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,21 @@
-angular.module('video-player').controller('ListCtrl', function ListCtrl($scope) {
+angular.module('video-player').controller('ListCtrl', function ListCtrl($scope, youTube) {
   //this.videos = window.exampleVideoData;
   //'this' references ctrl in html
-  $scope.queryData = function(query) {
-    console.log('inside queryData');
-  };
-  $scope.videos = window.exampleVideoData;
-  $scope.currentVideo = $scope.videos[0];
   //$scope references global angular window
   $scope.updateCurrentVideo = function(video) {
     $scope.currentVideo = video;
   };
+  $scope.updateVideos = function(videos) {
+    $scope.videos = videos;
+    $scope.updateCurrentVideo($scope.videos[0]);
+  };  
+  $scope.queryData = function(query) {
+    youTube.getVideos(query).then(function(response) {
+      $scope.updateVideos(response);
+    });
+  };
+  $scope.updateVideos(window.exampleVideoData);
 })
 .component('app', {
   templateUrl: './src/templates/app.html'
-  
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,11 +1,12 @@
 angular.module('video-player')
-.service('youTube', function($http) {
+.service('youTube', function($http, $window) {
   this.getVideos = function(query) {
+    
     return $http({
       method: 'GET',
       url: 'https://www.googleapis.com/youtube/v3/search',
-      data: {
-        key: window.YOUTUBE_API_KEY,
+      params: {
+        key: $window.YOUTUBE_API_KEY,
         q: query,
         maxResults: 5,  
         part: 'snippet',
@@ -15,7 +16,8 @@ angular.module('video-player')
     }).then(function successCallback(response) {
     // this callback will be called asynchronously
     // when the response is available
-      console.log("video received", response);
+      console.log("video received", response.data.items);
+      return response.data.items;
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
